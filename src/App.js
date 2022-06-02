bd4e1b005fdddd13bbad4a5fd6ac31557de73577
DONGRI PAVAN KUMAR <pavankumardongri@gmail.com>
2022-06-03 04:50:23 +0530

    solved routing maybe ...

--- src/App.js
+++ src/App.js
@@ -2,39 +2,9 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import Home from './components/pages/Home';
-import Cardiology from './components/pages/Cardiology';
-import Radiology from './components/pages/Radiology';
-import Ambulance from './components/pages/Ambulance';
-import Laboratory from './components/pages/Laboratory';
-import Managers from './components/pages/Managers';
-import Doctors from './components/pages/Doctors';
-import ContactUs from './components/pages/ContactUs';
-import SignUp from './components/pages/SignUp';
-
 function App() {
   return (
-    <Router>
       <Navbar />
-      <Routes>
-        <Route path='/' exact element={<Home/>} />
-        <Route path="/lab" exact element={<Laboratory/>} />
-        <Route path="/ambulance" exact element={<Ambulance/>} />
-
-        <Route path="/cardiology" exact element={<Cardiology/>} />
-        <Route path="/radiology" exact element={<Radiology/>} />
-
-        <Route path="/managers" exact element={<Managers/>} />
-        <Route path="/doctors" exact element={<Doctors/>} />
-
-        <Route path="/contactUs" exact element={<ContactUs/>} />
-
-        <Route path="/sign-up" exact element={<SignUp/>} />
-
-      </Routes>
-    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import Home from './components/pages/Home';
+import Cardiology from './components/pages/Cardiology';
+import Radiology from './components/pages/Radiology';
+import Ambulance from './components/pages/Ambulance';
+import Laboratory from './components/pages/Laboratory';
+import Managers from './components/pages/Managers';
+import Doctors from './components/pages/Doctors';
+import ContactUs from './components/pages/ContactUs';
+import SignUp from './components/pages/SignUp';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
         <App />
+        <Routes>
+            <Route path='/' exact element={<Home/>} />
+            <Route path="/lab" exact element={<Laboratory/>} />
+            <Route path="/ambulance" exact element={<Ambulance/>} />
+
+            <Route path="/cardiology" exact element={<Cardiology/>} />
+            <Route path="/radiology" exact element={<Radiology/>} />
+
+            <Route path="/managers" exact element={<Managers/>} />
+            <Route path="/doctors" exact element={<Doctors/>} />
+
+            <Route path="/contactUs" exact element={<ContactUs/>} />
+
+            <Route path="/sign-up" exact element={<SignUp/>} />
+        </Routes>
     </BrowserRouter>
 );
 
